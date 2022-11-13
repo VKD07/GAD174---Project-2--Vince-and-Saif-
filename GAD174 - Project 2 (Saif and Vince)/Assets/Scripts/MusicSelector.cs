909bce1e1bbc9405ec1431b74084e212ef89cb08
SaifAl75 <s.almheiri@student.saedubai.com>
2022-11-13 17:03:08 +0400

    Portal Color and Music
    I've added the ability to change the color of the portal and with it the Music.

---
+++ GAD174 - Project 2 (Saif and Vince)/Assets/Scripts/MusicSelector.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSelector : MonoBehaviour
+{
+    [SerializeField] AudioSource[] musicList;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            musicList[0].Stop();
+            musicList[1].Play();
+            musicList[2].Stop();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            musicList[0].Stop();
+            musicList[1].Stop();
+            musicList[2].Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            musicList[0].Stop();
+            musicList[1].Stop();
+            musicList[2].Play();
+        }
+    }
+}
